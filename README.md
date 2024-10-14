27a840040c1ce258e395d43370c5504f8f2b164e
Pueeeth <punithreddy417@gmail.com>
2024-10-14 14:50:20 +0530

    ball and courtpoints

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Project: Tennis Analysis Using Python
 Started: 1-10-2024(added input_videos)
 from (4-10 to 11-10) - Training Models for predition
 (13-10) - predicted players eliminating accurary or score number for each player   ---->  players with boxes x,y axis 
+(14-10) - predicted tennis-ball and court keypoints ----> court keypoints is detected to single frame beacuse only single court is presented throughout the video
 
 
 Goals:
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from utils import (read_video,
                    save_video)
-from trackers import PlayerTracker
+from trackers import PlayerTracker,BallTracker
+from court_line_detector import CourtLineDetector
+import cv2
 
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
@@ -10,15 +12,37 @@ def main():
     input_video_path = "input_videos/input_video.mp4"
     video_frames = read_video(input_video_path)
     
-    #Detecting Players
+    #Detecting Players and Ball
     player_tracker = PlayerTracker(model_path = 'yolov8x')
-    player_detections = player_tracker.detect_frames(video_frames,read_from_stub=False,stub_path="tracker_stubs/player_detections.pkl")
+    ball_tracker = BallTracker(model_path='models/yolov5_last.pt')
+    player_detections = player_tracker.detect_frames(video_frames,
+                                                     read_from_stub=True,
+                                                     stub_path="tracker_stubs/player_detections.pkl")
+    ball_detections = ball_tracker.detect_frames(video_frames,
+                                                     read_from_stub=True,
+                                                     stub_path="tracker_stubs/ball_detections.pkl")
+    ball_detections = ball_tracker.interpolate_ball_positions(ball_detections)
+    
+    
+    #Court Line Detector model
+    court_model_path = "models/keypoints_model.pth"
+    court_line_detector = CourtLineDetector(court_model_path)
+    court_keypoints = court_line_detector.predict(video_frames[0])
     
     
     #Draw output
     
     ##Draw Player Bounding Boxes
     output_video_frames = player_tracker.draw_bboxes(video_frames,player_detections)
+    output_video_frames = ball_tracker.draw_bboxes(video_frames,ball_detections)
+    
+    ##Draw court KeyPoints
+    output_video_frames = court_line_detector.draw_keypoints_on_video(output_video_frames,court_keypoints)
+    
+    ## Draw frame number on top left corner
+    for i, frame in enumerate(output_video_frames):
+        cv2.putText(frame, f"Frame: {i}",(10,30),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    
     
     save_video(output_video_frames, "output_videos/output_video.avi")
     
